3a625ec24f47f78eb97b0f356cf122fff083e02d
rksgr <r_kjha@yahoo.com>
2021-12-18 14:39:29 +0530

    Added: Parent and Child class

---
+++ AccountBalance.java
@@ -0,0 +1,13 @@
+
+package com.mycompany.dec_16_bank_account;
+
+public class AccountBalance {
+    private double acc_bal;
+    
+    public double getAcc_bal(){
+        return acc_bal;
+    }
+    public void setAcc_bal(double acc_bal){
+        this.acc_bal = acc_bal;
+    }
+}
